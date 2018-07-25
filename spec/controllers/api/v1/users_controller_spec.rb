8836a560610a3296dffe40c4203efa47f9221e87
Corey Soreff <csoreff@gmail.com>
2018-07-25 16:24:49 +0900

    remove unnecessary code

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -13,17 +13,4 @@ describe Api::V1::UsersController do
       expect(response.code).to eq('200')
     end
   end
-
-  describe "POST #create" do
-    before(:each) do
-      @user = FactoryBot.create :user
-      auth_headers = @user.create_new_auth_token
-      request.headers.merge!(auth_headers)
-      get :show, params: {id: @user.id}
-    end
-
-    it 'responds with 200 status code' do
-      expect(response.code).to eq('200')
-    end
-  end
 end
